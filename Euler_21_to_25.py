cd31dd25be5df5d08b6feb5bd46944fa50519640
ToLearnDataScience <86862068+ToLearnDataScience@users.noreply.github.com>
2021-07-18 17:50:46 +0900

    Update Euler_21_to_25.py

--- Euler_21_to_25.py
+++ Euler_21_to_25.py
@@ -50,3 +50,80 @@ print(answer)
 => "220 - 284" | "1184 - 1210" | "2620 - 2924" | "5020 - 5564" | "6232 - 6386"
 '''
 
+
+
+
+
+
+
+
+
+
+"""
+No.22 [Names scores]
+
+Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, 
+begin by sorting it into alphabetical order. 
+Then working out the alphabetical value for each name, 
+multiply this value by its alphabetical position in the list to obtain a name score.
+
+For example, when the list is sorted into alphabetical order, 
+COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. 
+So, COLIN would obtain a score of 938 × 53 = 49714.
+
+What is the total of all the name scores in the file?
+"""
+import re
+
+
+with open("name.txt", mode ='r', encoding = "utf-8") as f :
+    data = f.read()
+    
+type(data) # str
+
+# str -> list
+pre_data = data.split(",")
+type(pre_data) # list
+
+final_data = []
+for s in pre_data :
+    st = re.findall("[A-Z]+", s)
+    final_data.extend(st)
+
+print(final_data)
+# ['MARY', 'PATRICIA', 'LINDA', 'BARBARA', 'ELIZABETH', 'JENNIFER', 'MARIA', 'SUSAN', ...
+# 'LYNWOOD', 'JERE', 'HAI', 'ELDEN', 'DORSEY', 'DARELL', 'BRODERICK', 'ALONSO']
+
+'''
+ASCII CODE - Alphabet
+
+65 - A
+66 - B
+67 - C
+   .
+   .
+   .
+88 - X
+89 - Y
+90 - Z
+'''
+
+final_data.sort()
+
+total_num = 0
+
+for idx, value in enumerate(final_data) :
+    num_sum = 0
+    for st in value :
+        num_sum += ord(st) - 64
+    total_num += (num_sum * (idx+1))
+    
+        
+
+print(total_num) # 871198282
+
+'''
+[The answer]
+871198282
+'''
+
