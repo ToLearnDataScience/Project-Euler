f2fcae71b2f50a55a763a56965168ed6cc219bb4
ToLearnDataScience <86862068+ToLearnDataScience@users.noreply.github.com>
2021-07-19 10:14:30 +0900

    Update Euler_21_to_25.py

--- Euler_21_to_25.py
+++ Euler_21_to_25.py
@@ -127,3 +127,72 @@ print(total_num) # 871198282
 871198282
 '''
 
+
+
+
+
+
+
+
+
+
+"""
+No.23 [Non-abundant sums]
+
+A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. 
+For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+
+A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
+
+As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, 
+the smallest number that can be written as the sum of two abundant numbers is 24. 
+
+By mathematical analysis, 
+it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. 
+
+However, this upper limit cannot be reduced any further by analysis 
+even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
+
+Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+"""
+
+
+def find_abundant_numbers(number) :
+    abundant_list = []
+    for i in range(2, number) :
+        divisor_list = []
+        div_sum = 0
+        for k in range(1, i) :
+            if i % k == 0 :
+                divisor_list.append(k)
+        for div in divisor_list :
+            div_sum += div
+        if div_sum > i :
+            abundant_list.append(i)
+    return abundant_list
+                
+abundant = find_abundant_numbers(28123)
+print(abundant)
+len(abundant) # 6965
+
+sum_list = []
+for i in range(len(abundant)) :
+    for j in range(len(abundant)) :
+        if abundant[i] + abundant[j] < 28123 :
+            sum_list.append(abundant[i] + abundant[j])
+
+sum_set = set(sum_list)
+sum_list2 = sum_set
+len(sum_list2) # 26666
+
+answer = 0
+for i in range(1, 28123) :
+    if i not in sum_list2 :
+        answer += i
+
+print(answer) # 4179871
+
+'''
+[The answer]
+4179871
+'''
